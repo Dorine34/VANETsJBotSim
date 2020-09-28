ecd9ac0162e22672c9b3732e245f304750b03b77
Dorine34 <dorinetabary@gmail.com>
2020-09-28 11:30:01 +0200

    SA with 100 nodes

---
+++ algoDoubleSelection/Prob.java
@@ -0,0 +1,55 @@
+package algoDoubleSelection;
+
+public class Prob {
+	Integer demande ;
+	HighwayCar car;
+	double q;
+	double y;
+	long tempsReception;
+	
+	
+	public Prob(Integer demande, HighwayCar car, float q, float y, long tempsReception) {
+		super();
+		this.demande = demande;
+		this.car = car;
+		this.q = q;
+		this.y = y;
+		this.tempsReception=tempsReception;
+	}
+	@Override
+	public String toString() {
+		return "Prob [demande=" + demande + ", car=" + car + 
+				", q=" + q + ", y=" + y + " temps = "+this.tempsReception +"]";
+	}
+	public HighwayCar getCar() {
+		return car;
+	}
+	public long getTempsReception() {
+		return tempsReception;
+	}
+	public void setTempsReception(long tempsReception) {
+		this.tempsReception = tempsReception;
+	}
+	public void setCar(HighwayCar car) {
+		this.car = car;
+	}
+	public Integer getDemande() {
+		return demande;
+	}
+	public void setDemande(Integer demande) {
+		this.demande = demande;
+	}
+	public double getQ() {
+		return q;
+	}
+	public void setQ(double d) {
+		this.q = d;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double d) {
+		this.y = d;
+	}
+	
+}
